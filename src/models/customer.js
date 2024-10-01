e7fb3434faff4320e8273026b82a16db37d951e6
LimbergDV <limbergdelvalle@gmail.com>
2024-10-01 11:27:15 -0600

    Customer Model Finished

--- src/models/customer.js
+++ src/models/customer.js
@@ -5,12 +5,35 @@ const sequelize =require("../config/connection.js");
 //Define Table CUSTOMER
 const Customer = sequelize.define(
     "Customer",{
-        id_customer:{
+        id_customer: {
             type:DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
         },
-        
+        name: {
+            type:DataTypes.STRING(45),
+            allowNull: false,
+        },
+        last_names: {
+            type:DataTypes.STRING(45),
+            allowNull:false,
+        },
+        phone_number: {
+            type:DataTypes.STRING(12),
+            allowNull:false,
+        },
+        CURP: {
+            type: DataTypes.STRING(18),
+            allowNull: false,
+        },
+        number_licese:{
+            type: DataTypes.INTEGER(15),
+            allowNull: false,
+        },
+        birthdate: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
     }
 );
